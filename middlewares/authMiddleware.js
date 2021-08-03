d2c887f373d75f649bc0d464cd0fc322b97061e3
Zulfikar <zulfikarrosadi31@gmail.com>
2021-08-03 07:13:29 +0700

    adding logout button BAD : authMiddleware return error ERR_HTTP_HEADERS_SENT

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,9 +4,8 @@ const requireAuth = (req, res, next) => {
   const token = req.cookies.jerawat;
   if (token) {
     verify(token, process.env.SECRET_KEY, (err) => {
-      if (err) res.redirect('/login');
-
-      next();
+      if (err) return res.redirect('/login');
+      return next();
     });
   }
   return res.redirect('/login');
